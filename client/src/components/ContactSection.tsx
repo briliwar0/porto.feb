7ca6b32f101c5db2ef4b29acf2c4654de78139cd
Replit Replit <40515004-paprespapres2@users.noreply.replit.com>
2025-04-09 07:15:42 +0000

    Update changes made to the project

--- client/src/components/ContactSection.tsx
+++ client/src/components/ContactSection.tsx
@@ -177,7 +177,7 @@ const ContactSection = () => {
                           <FormLabel>Your Name</FormLabel>
                           <FormControl>
                             <Input 
-                              placeholder="Febri" 
+                              placeholder="name" 
                               className="bg-white/5 dark:bg-dark/30 border border-gray-300 dark:border-gray-700"
                               {...field} 
                             />
@@ -195,7 +195,7 @@ const ContactSection = () => {
                           <FormLabel>Your Email</FormLabel>
                           <FormControl>
                             <Input 
-                              placeholder="febri@example.com" 
+                              placeholder="name@example.com" 
                               type="email"
                               className="bg-white/5 dark:bg-dark/30 border border-gray-300 dark:border-gray-700"
                               {...field} 
